2b6e2d262741c2327133baa98336e7648ef0882c
DurgaprasadKolli <kolli.durgaprasad8000@gmail.com>
2024-12-28 13:08:15 +0530

    Users Changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const authRoutes = require('./authRoute');
 const batchnumberRoutes = require('./batchnumberRoute');
 const transactionRoutes = require('./transactionRoutes');
+const usersRoutes = require('./usersRoutes');
 
 
 const router = express.Router();
@@ -12,6 +13,7 @@ router.get('/health-check', (req, res) => res.send('OK'));
 router.use('/auth', authRoutes);
 router.use('/batchNumbers', batchnumberRoutes);
 router.use('/transaction', transactionRoutes);
+router.use('/users', usersRoutes);
 
 
 module.exports = router;
