4ad373d30745798b8b975a822bfa10aa19f24528
PuttaSurya <suryapavan7a77@gmail.com>
2025-02-05 09:29:04 +0530

    userandproductofferchanges

--- models/productOffers.model.js
+++ models/productOffers.model.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const productOffersSchema = new mongoose.Schema({
     productOfferImageUrl: { type: String },
     productOfferDescription: { type: String, required: true },
-    productOfferTitle: { type: String, required: true },
+    // productOfferTitle: { type: String, required: true }, -->
     validUntil: { type: Date, required: true},
     productOfferStatus: { type: String, required: true },
     updatedBy: { type: String },
     createdBy: { type: String },
+    cashback: { type: Number, required: true, default: 0 }, 
+    redeemPoints: { type: Number, required: true, default: 0 },
 }, { timestamps: true });
 
 const productOffers = mongoose.model('productOffers', productOffersSchema, 'productOffers');
