f31752bf224dc6c725ffc1d2f9f21f72eca53add
srinikandula <srini.kandula@whizzard.in>
2025-03-04 11:02:10 +0530

    Adding logging to the project.

--- models/Transaction.js
+++ models/Transaction.js
@@ -6,15 +6,15 @@ const transactionSchema = new mongoose.Schema({
     batchId: { type: mongoose.Schema.Types.ObjectId, ref: 'Batch', required: true },
     createdBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     updatedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    qr_code_id: {type: String},
+    //qr_code_id: {type: String},
     qr_code: { type: String, required: true, unique: true },
-    couponValue: { type: Number },
-    points: { type: Number },
+    //couponValue: { type: Number },
+    //points: { type: Number },
     redeemablePoints: { type: Number },
     value: { type: Number },
     couponCode: { type: Number },
-    redeemedBy: { type: String },// user who redeemed this coupon
-    redeemedByMobile: { type: String },
+    //redeemedBy: { type: String },// user who redeemed this coupon
+    //redeemedByMobile: { type: String },
     UDID: { type: String },
     isProcessed: { type: Boolean, default: false },  // Default set to false
     pointsRedeemedBy: { type: String },
